964d7ee731ee773209899b177836b60cd82abb5f
muhyta <muhyta@gmail.com>
2020-04-10 18:40:51 +0500

    point

---
+++ oop-lesson-snake/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_lesson_snake
+{
+    class Point
+    {
+        internal int x;
+        internal int y;
+        internal char sym;
+
+        internal void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
