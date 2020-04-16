1874f446fd24bdb9c9ff074d7313df0fb25d052b
muhyta <muhyta@gmail.com>
2020-04-17 01:17:54 +0500

    5 - RAM

--- oop-lesson-snake/Program.cs
+++ oop-lesson-snake/Program.cs
@@ -6,15 +6,50 @@ namespace oop_lesson_snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(2, 3, '*');
-            p1.Draw();
+            int x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = "+x);
 
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
+
+            Point p1 = new Point(1,3,'*');
+            Move(p1,10,10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            Point p2 = new Point(3, 5, '#');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            p1 = new Point(1, 3, '*');
+            Reset(p1);
+            Console.WriteLine("Call Reset. p1.x = " + p1.x + ", p1.y = " + p1.y);
 
             Console.ReadKey();
         }
 
-        
+        private static void Reset(Point p)
+        {
+            p = new Point();
+        }
+
+        private static void Move(Point p, int dx, int dy)
+        {
+            p.x += dx;
+            p.y += dy;
+        }
+
+        private static void Func3(int x)
+        {
+            x = x + 1;
+        }
+
+        private static void Func2(int value)
+        {
+            value = value + 1;
+        }
     }
 }
