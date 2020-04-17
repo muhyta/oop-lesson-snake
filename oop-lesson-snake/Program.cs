4e3093be05d3287b9d09ea1e072a7cf5647b6d81
muhyta <muhyta@gmail.com>
2020-04-18 01:55:21 +0500

    vertical horizontal lines

--- oop-lesson-snake/Program.cs
+++ oop-lesson-snake/Program.cs
@@ -9,8 +9,14 @@ namespace oop_lesson_snake
             Point p1 = new Point(2, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point(4,5,'#');
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            HorizontalLine hl1 = new HorizontalLine(5, 10, 8, '+');
+            hl1.Draw();
+
+            VerticalLine vl1 = new VerticalLine(5, 8, 10, '+');
+            vl1.Draw();
 
             Console.ReadKey();
         }
---
+++ oop-lesson-snake/VerticalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_lesson_snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        internal VerticalLine(int x, int yUp, int yDown, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        internal void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
