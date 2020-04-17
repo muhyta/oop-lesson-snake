80f92e9c5a53e960fe1b624fbda9c41b959be64a
muhyta <muhyta@gmail.com>
2020-04-18 02:05:17 +0500

    lines

--- oop-lesson-snake/Program.cs
+++ oop-lesson-snake/Program.cs
@@ -6,18 +6,19 @@ namespace oop_lesson_snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(120, 30);
+            HorizontalLine hl1 = new HorizontalLine(0, 119, 0, '+');
+            hl1.Draw();
+            HorizontalLine hl2 = new HorizontalLine(0, 119, 28, '+');
+            hl2.Draw();
+            VerticalLine vl1 = new VerticalLine(0, 0, 28, '+');
+            vl1.Draw();
+            VerticalLine vl2 = new VerticalLine(119, 0, 28, '+');
+            vl2.Draw();
             Point p1 = new Point(2, 3, '*');
             p1.Draw();
-
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-
-            HorizontalLine hl1 = new HorizontalLine(5, 10, 8, '+');
-            hl1.Draw();
-
-            VerticalLine vl1 = new VerticalLine(5, 8, 10, '+');
-            vl1.Draw();
-
             Console.ReadKey();
         }
 
